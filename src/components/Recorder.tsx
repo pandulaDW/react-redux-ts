28a8d733ac7e715a81d3821cd8b37e4b5c6aa625
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2020-12-12 16:37:56 +0530

    Finished recorder component UI parts

--- src/components/Recorder.tsx
+++ src/components/Recorder.tsx
@@ -1,24 +1,51 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import cx from "classnames";
-import { start, selectDateStart } from "../redux/recorder";
+import { start, stop, selectDateStart } from "../redux/recorder";
 import "../styles/Recorder.css";
+
+const addZero = (num: number): string => (num < 10 ? `0${num}` : `${num}`);
 
 const Recorder = () => {
   const dispatch = useDispatch();
   const dateStart = useSelector(selectDateStart);
   const started = dateStart !== "";
+  let interval = React.useRef<number>(0); // to make interval variable persists after render
+  const [, setCount] = React.useState<number>(0); // force a re-render at each interval
 
   const handleClick = () => {
-    dispatch(start());
+    if (started) {
+      window.clearInterval(interval.current);
+      dispatch(stop());
+    } else {
+      dispatch(start());
+      interval.current = window.setInterval(() => {
+        setCount((count) => count + 1);
+      }, 1000);
+    }
   };
+
+  // clearing the interval when unmounting the element from the dom
+  React.useEffect(() => {
+    return () => window.clearInterval(interval.current);
+  }, []);
+
+  let seconds = started
+    ? Math.floor((Date.now() - new Date(dateStart).getTime()) / 1000)
+    : 0;
+  const hours = seconds ? Math.floor(seconds / 60 / 60) : 0;
+  seconds -= hours * 60 * 60;
+  const minutes = seconds ? Math.floor(seconds / 60) : 0;
+  seconds -= minutes * 60;
 
   return (
     <div className={cx("recorder", { "recorder-started": started })}>
       <button onClick={handleClick} className="recorder-record">
         <span></span>
       </button>
-      <div className="recorder-counter">00:00:00</div>
+      <div className="recorder-counter">{`${addZero(hours)}:${addZero(
+        minutes
+      )}:${addZero(seconds)}`}</div>
     </div>
   );
 };
